0405f3aa8db0ee49002037c223c634645f573d46
Takanori Suzuki <takanori@takanory.net>
2023-01-18 22:35:03 +0900

    add script

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # pyconjptv-subtitle
+
 PyCon JP TVの字幕ファイルを生成するスクリプト
+
+## 環境構築
+
+```bash
+$ brew install ffmpeg
+$ python3.10 -m venv env
+$ . env/bin/activate
+(env) $ pip install -r requirements.txt
+```
+
+## スクリプトを実行
+
+* 引数にエピソードの番号を指定する(`24` 等)
+* `python -u` で実行する実行中処理結果が出力される
+
+```bash
+(env) $ python -u pyconjptv-subtitle 24
+(env) $ ls *.srt
+pyconjptv24.srt
+```
+
+## 参考
+
+* [yt-dlp/yt-dlp: A youtube-dl fork with additional features and fixes](https://github.com/yt-dlp/yt-dlp)
+* [openai/whisper: Robust Speech Recognition via Large-Scale Weak Supervision](https://github.com/openai/whisper)
---
+++ pyconjptv-subtitle.py
@@ -0,0 +1,78 @@
+"""
+PyCon JP TVのエピソード番号を指定すると、音声をダウンロードして
+whisperでsrtファイルを生成するスクリプト
+
+usage: python -u pyconjptv-subtitle.py 24
+"""
+
+import re
+import sys
+from pathlib import Path
+from urllib.error import HTTPError
+from urllib.request import urlopen
+
+import yt_dlp
+from whisper import load_model
+from whisper.utils import write_srt
+
+
+def download_webm(url: str, webm: str) -> int:
+    """yt_dlpでYouTubeの音声をwebm形式でダウンロード"""
+    print("--- download youtube audio file ---")
+    ydl_opts = {
+        "format": "bestaudio/best",
+        "outtmpl": {"default": webm},
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        error_code = ydl.download(url)
+
+    return error_code
+
+
+def generate_srt(audio_file: str) -> None:
+    """whisperで音声ファイルから字幕生成"""
+    print("--- transcribe audio file ---")
+    model = load_model("large-v2")
+    result = model.transcribe(audio_file, verbose=True, language="japanese")
+
+    # 結果をsrtファイルに出力
+    base, _ = audio_file.rsplit(".", 1)
+    srt_file = Path(f"{base}.srt")
+    with srt_file.open(mode="w", encoding="utf-8") as srt:
+        write_srt(result["segments"], file=srt)
+
+
+def main(num: str) -> None:
+    # 実行中の状態を出力するために標準出力、エラー出力を行バッファリングにする
+    sys.stdout.reconfigure(line_buffering=True)
+    sys.stderr.reconfigure(line_buffering=True)
+
+    # PyCon JP TVのWebページからyoutubeのURLを取得
+    url = f"https://tv.pycon.jp/episode/{num}.html"
+    try:
+        with urlopen(url) as f:
+            html = f.read().decode("utf-8")
+    except HTTPError:
+        print("指定されたPyCon JP TVのページが存在しません")
+        return
+
+    if m := re.search(r'src="https://www.youtube.com/embed/([^"]+)"', html):
+        youtube_url = f"https://www.youtube.com/watch?v={m[1]}"
+    else:
+        print("Youtube linkが見つかりませんでした")
+        return
+
+    basename = f"pyconjptv{num}"
+    webm = f"{basename}.webm"
+    error_code = download_webm(youtube_url, webm)
+
+    # whisperで字幕をsrtファイルに生成
+    generate_srt(webm)
+
+
+if __name__ == "__main__":
+    # 最初の引数を渡す
+    if len(sys.argv) >= 2:
+        main(sys.argv[1])
+    else:
+        pass
